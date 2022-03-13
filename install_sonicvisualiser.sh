fada177ba95e913594af79aa668c7e70f051f7d1
Alex <89443340+kansasdynamics@users.noreply.github.com>
2022-03-12 16:59:38 -0800

    Update install_sonicvisualiser.sh

--- install_sonicvisualiser.sh
+++ install_sonicvisualiser.sh
@@ -3,15 +3,13 @@
 DIRECTORY=/home/kali/Tools
 if [[ -d "$DIRECTORY" ]]
   then
-      # Code for if /home/kali/Tools exists
-      # echo "$DIRECTORY exists on your filesystem."
-      # sudo apt install libimage-exiftool-perl
+      # If /home/kali/Tools exists then cd and install
       cd $HOME'/Tools'
       wget https://code.soundsoftware.ac.uk/attachments/download/2810/sonic-visualiser_4.4_amd64.deb
       sudo apt install ./sonic-visualiser_4.4_amd64.deb
       sonic-visualiser
   else
-      # Code for if /home/kali/Tools doesn't exist
+      # If /home/kali/Tools doesn't exist then mkdir, cd, and install
       cd $HOME
       mkdir Tools
       cd ./Tools
