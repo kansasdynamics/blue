41018770a31378ecf17d56e624a6d4ccff0f67e8
Alex <89443340+kansasdynamics@users.noreply.github.com>
2022-10-07 21:40:20 -0700

    Update add_host.sh

--- add_host.sh
+++ add_host.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Add a new entry to the host file
 read -p "IP Address: " ADDR
 read -p "Hostname: " HOST
 echo "${ADDR} ${HOST}" | sudo tee -a /etc/hosts
+echo "Successfully updated host file."
+cat /etc/hosts
